1f506aeabff8ac8ae5c1f87957c540862e785484
Andrii Litvynchuk <andrii.litvynchuk@leiainc.com>
2026-03-15 19:04:52 +0200

    update tests

--- tests/test_sensor.cpp
+++ tests/test_sensor.cpp
@@ -1,14 +1,16 @@
+#include <iostream>
+
 #include "start/sensor.h"
 
 int main() {
     CameraSensor cam;
-    if (cam.name() != "Camera") return 1;
-    if (cam.read_frame() != 0) return 1;
-    if (cam.read_frame() != 1) return 1;
+    if (cam.name() != "Camera") throw std::runtime_error("Camera name is not Camera");
+    if (cam.read_frame() != 0) throw std::runtime_error("Camera read frame is not 0");
+    if (cam.read_frame() != 1) throw std::runtime_error("Camera read frame is not 1");
 
     DummySensor dummy;
-    if (dummy.name() != "Dummy") return 1;
-    if (dummy.read_frame() != -1) return 1;
-
+    if (dummy.name() != "Dummy") throw std::runtime_error("Dummy name is not Dummy");
+    if (dummy.read_frame() != -1) throw std::runtime_error("Dummy read frame is not -1");
+    std::cout << "All tests passed" << std::endl;
     return 0;
 }
